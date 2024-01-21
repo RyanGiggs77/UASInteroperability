8c5739cb77fcc1ad055d2cac21e323f9361c9a47
ezbim <bhima.pilot@gmail.com>
2024-01-21 11:43:36 +0700

    Book Review Controller

---
+++ EBook/app/Http/Controllers/BookReviewController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookReview; // Adjust the namespace to reflect the correct location
+use Illuminate\Http\Request;
+
+class BookReviewController extends Controller
+{
+    public function index()
+    {
+        return BookReview::all();
+
+    }
+
+    public function show($id)
+    {
+        return BookReview::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return BookReview::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $bookReview = BookReview::find($id);
+        $bookReview->update($request->all());
+        return $bookReview;
+    }
+
+    public function destroy($id)
+    {
+        BookReview::find($id)->delete();
+        return response()->json(['message' => 'Record deleted successfully']);
+    }
+}
